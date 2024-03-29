411474923edd7d0ccc8ecc35ccd78f4bb3075d18
ansxor <darienreese@gmail.com>
2024-03-29 11:05:43 -0400

    Got Ruby mostly working?

--- init.el
+++ init.el
@@ -14,3 +14,15 @@
 
 (answer-config/load-config answer-config/secret-config-location)
 (answer-config/load-config answer-config/config-location)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-vc-selected-packages '((asdf :url "https://github.com/tabfugnic/asdf.el"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
