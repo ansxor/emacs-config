8e161a970706d91b5cde5e61ebb10d9c55b4cec1
ansxor <darienreese@gmail.com>
2024-04-04 14:13:54 -0400

    Added new message where it will display if copilot is querying

--- code/copilot-posframe.el
+++ code/copilot-posframe.el
@@ -10,7 +10,7 @@
   "*copilot-message-buffer*"
   "The Posframe buffer which contains the message for completions")
 
-(defun copilot-complete-custom-fail (fail-function)
+(defun copilot-complete-custom-fail (success-function fail-function)
   "Complete at the current point."
   (interactive)
   (setq copilot--last-doc-version copilot--doc-version)
@@ -22,7 +22,9 @@
    (jsonrpc-lambda (&key completions &allow-other-keys)
      (let ((completion (if (seq-empty-p completions) nil (seq-elt completions 0))))
        (if completion
-           (copilot--show-completion completion)
+			  (progn
+				 (copilot--show-completion completion)
+				 (funcall success-function))
 			(funcall fail-function))))))
 
 (defun copilot-posframe-hide ()
@@ -32,6 +34,14 @@
 	 (posframe-delete copilot-posframe-buffer)
 	 (setq copilot-posframe-point nil)))
 
+(defun copilot-posframe-show-message (msg)
+  "Show `msg' at the point."
+  (posframe-show copilot-posframe-buffer
+					  :string msg
+					  :position (point)
+					  :timeout 3
+					  :internal-border-color "#ffffff") )
+
 (defun copilot-posframe-flow ()
   "Depending on whether the Copilot overlay is visible, either request for a
 completion, or accept the completion available."
@@ -40,13 +50,13 @@ completion, or accept the completion available."
 	 (posframe-delete copilot-posframe-buffer))
   (if (copilot--overlay-visible)
 		(copilot-accept-completion)
-	 (copilot-complete-custom-fail '(lambda ()
-												 (setq copilot-posframe-point (point))
-												 (posframe-show copilot-posframe-buffer
-																	 :string "No completion available"
-																	 :position (point)
-																	 :timeout 3
-																	 :internal-border-color "#ffffff")))))
+	 (progn
+		(copilot-posframe-show-message "Querying Copilot...")
+		(copilot-complete-custom-fail
+		 '(lambda () (posframe-delete copilot-posframe-buffer))
+		 '(lambda ()
+			 (setq copilot-posframe-point (point))
+			 (copilot-posframe-show-message "No completion available"))))))
 
 (defvar copilot-posframe-mode-map (make-sparse-keymap)
   "Keymap used by `copilot-posframe-mode'.")
